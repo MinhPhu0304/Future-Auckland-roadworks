997c9944ec55450627ae54496a073313e03958cb
minh <Lminhphu0304@gmail.com>
2020-01-19 16:35:13 +1300

    Add Root level context

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer } from 'react'
+
+import './App.css'
+import { Map } from './components/map'
+import { FetchScheduledWork } from './components/DataFetching'
+
+export const State = React.createContext(null)
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [state, dispatch] = useReducer(reducers, {})
+return (
+    <State.Provider value={{dispatch, state}}>
+      <div className="App">
+        <Map state={state}/>
+        <FetchScheduledWork/>
+      </div>
+    </State.Provider>
   );
+}
+
+function reducers (state, payload) {
+  const { action } = payload
+
+  switch (action) {
+    case 'Change':
+      const { locationData } = payload
+      return {
+        ...state,
+        locationData 
+      }
+    default:
+      return state
+  }
 }
 
 export default App;
