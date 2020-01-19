4571f4afa2997543de6346ccaf062460bc4a4591
minh <Lminhphu0304@gmail.com>
2020-01-19 16:35:47 +1300

    Remove unused components

--- src/components/map/index.jsx
+++
@@ -1,8 +0,0 @@
-import mapboxgl from 'mapbox-gl'
-
-mapboxgl.accessToken = process.env.MAP_BOX_TOKEN || 'GET YOUR OWN TOKEN YOU LAZY'
-const map = new mapboxgl.Map({
-container: 'YOUR_CONTAINER_ELEMENT_ID',
-style: 'mapbox://styles/mapbox/streets-v11'
-});
-
