5469e434e7fba28bb52541e7059905248359a1ce
minh <Lminhphu0304@gmail.com>
2020-02-27 18:46:16 +1300

    Replace Mapbox components to Leaflet

--- src/components/map/index.js
+++ src/components/map/index.js
@@ -1,84 +1,41 @@
-import React, { useState, useContext } from 'react'
-import DeckGL, { ScatterplotLayer } from 'deck.gl'
-import { StaticMap } from 'react-map-gl'
-import { isEmpty } from 'ramda'
+import React, { useContext } from 'react'
+import { Map as LeafletMap, Popup, Marker, TileLayer } from 'react-leaflet'
 
 import { State } from '../../App'
 
-const MAP_BOX_TOKEN = process.env.REACT_APP_MAP_BOX_TOKEN || 'Get your own token'
-
 export function Map () {
-  const [hoveringObject, setHovering] = useState({})
   const { state } = useContext(State)
   const { locationData } = state
-  
-  const layer = generateDataPayload(locationData)
-  const defaultLayerOptions = getDefaultLayerOption()
-  const scatteredPlotLayer = new ScatterplotLayer({
-    ...defaultLayerOptions,
-    data: layer,
-    getPosition: d => d.coordinates,
-    getRadius: d => Math.sqrt(d.exits),
-    getFillColor: d => [255, 140, 0],
-    onHover: ({object, x, y}) => {
-      if (object == null) {
-        setHovering({})
-      }  else {
-        setHovering({
-          point: object,
-          pointerX: x,
-          pointerY: y
-        })
-      }
-    }
-  })
+  const initialMapView = getInitMapView()
+  const position = [initialMapView.lat, initialMapView.lng]
   return (
-    <DeckGL initialViewState={getInitialMapView()} controller={true} layers={[scatteredPlotLayer]}>
-      <StaticMap mapStyle="mapbox://styles/mapbox/dark-v9"  mapboxApiAccessToken={MAP_BOX_TOKEN} />
-      { !isEmpty(hoveringObject) && <Tooltip markerData={hoveringObject} pointerX={hoveringObject.pointerX} pointerY={hoveringObject.pointerY}/>}
-    </DeckGL>
-  )
-}
-
-function generateDataPayload (locationData) {
-  if (!locationData || locationData.length == 0) return []
-  if (!Array.isArray(locationData)) return []
-  return locationData.map(location => {
-    return {
-      ...location,
-      coordinates: location.geometry.coordinates, 
-    }
-  })
-}
-
-function getDefaultLayerOption () {
-  return {
-    pickable: true,
-    opacity: 0.8,
-    stroked: true,
-    filled: true,
-    autoHighlight:  true,
-    radiusScale: 10,
-    radiusMinPixels: 10,
-    radiusMaxPixels: 1000,
-    highlightColorhighlightColor: 'blue',
-    lineWidthMinPixels: 1,
-  }
-}
-
-function getInitialMapView () {
-  return {
-    longitude: 174.764049,
-    latitude: -36.852096,
-    zoom: 14,
-  }
-}
-
-function Tooltip ({ markerData, pointerX, pointerY}) {
-  return (
-    <div style={{ position: 'absolute', zIndex: 1, pointerEvents: 'none', left: pointerX, top: pointerY, backgroundColor: 'white', maxWidth: '200px' }}>
-      <h6>{ markerData.point.name } </h6>
-      <p> { markerData.point.description }</p>
+    <div style={{ height: 575, width: 500 }}>
+      <LeafletMap center={position} zoom={initialMapView.zoom} style={{ height: 575, width: 500 }}>
+        <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+        />
+        {
+          locationData.map((location) => {
+            console.log(location.geometry.coordinates)
+            return (
+              <Marker position={[location.latitude, location.longitude]}>
+                <Popup>
+                  {location.name}
+                </Popup>
+              </Marker>
+            )
+          })
+        }
+      </LeafletMap>
     </div>
   )
+}
+
+function getInitMapView() {
+  return {
+    lat: -36.848461,
+    lng: 174.763336,
+    zoom: 10,
+  }
 }
