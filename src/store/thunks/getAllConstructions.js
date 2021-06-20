5d6281c1e7395baa7d418f916b9ed6468b14fce9
minh <Lminhphu0304@gmail.com>
2021-06-20 12:36:16 +1200

    Dispatch page state after fetch data

--- src/store/thunks/getAllConstructions.js
+++ src/store/thunks/getAllConstructions.js
@@ -1,19 +1,25 @@
-import { Constructions } from 'store/reducers'
+import { Constructions, PageState } from 'store/reducers'
+import { PAGE_STATE } from 'store/reducers/pageState'
 
-function getAllConstructions () {
+function getAllConstructions() {
   return async (dispatch) => {
-    const res = await fetch('https://api.at.govt.nz/v2/locations/scheduledworks', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'Ocp-Apim-Subscription-Key': `${process.env.REACT_APP_AT_TRANSPORT_KEY}`
-      }
-    })
-    const { response } = await res.json()
-    dispatch(Constructions.actions.UPDATE_CONSTRUCTIONS({ constructions: response }))
+    try {
+      const res = await fetch('https://api.at.govt.nz/v2/locations/scheduledworks', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Ocp-Apim-Subscription-Key': `${process.env.REACT_APP_AT_TRANSPORT_KEY}`
+        }
+      })
+      const { response } = await res.json()
+      dispatch(Constructions.actions.UPDATE_CONSTRUCTIONS({ constructions: response }))
+      dispatch(PageState.actions.UPDATE_PAGE_STATE({ newPageState: PAGE_STATE.LOADED }))
+    } catch (e) {
+      dispatch(PageState.actions.UPDATE_PAGE_STATE({ newPageState: PAGE_STATE.FAILED }))
+    }
   }
 }
 
-export { 
-  getAllConstructions
+export {
+  getAllConstructions,
 }
