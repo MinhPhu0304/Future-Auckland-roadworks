a118eaf5544629e2a402a23d52fe231a00e5ea95
minh <Lminhphu0304@gmail.com>
2021-06-20 12:35:44 +1200

    Add loading indicator component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux'
 
 import './App.css'
 import { Map } from 'components/map'
+import { LoadingIndicator } from 'components/loading'
 import { store } from 'store'
 import { getAllConstructions } from 'store/thunks'
 
@@ -15,6 +16,7 @@ export function App() {
 
   return (
     <Provider store={store}>
+      <LoadingIndicator />
       <div className="App">
         <Map />
       </div>
---
+++ src/components/loading/index.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+import { PAGE_STATE } from 'store/reducers/pageState'
+import './index.css'
+
+export const LoadingIndicator = connect(mapStateToProps)(LoadingDumb)
+
+function mapStateToProps({ pageState }) {
+  return {
+    pageState
+  }
+}
+
+function LoadingDumb({ pageState }) {
+  if (pageState === PAGE_STATE.LOADING) {
+    return (
+      <div class="lds-roller">
+        <div></div>
+        <div></div>
+        <div></div><div></div><div></div><div></div><div></div><div></div>
+        <p style={{ marginBottom: 100 }}>
+          Loading data...
+        </p>
+      </div>
+    )
+  }
+  if (pageState === PAGE_STATE.FAILED) {
+    return (
+      <div className="fail-loading__container">
+        <p>
+          Uh-oh! Looks like we could not get future road works
+        </p>
+      </div>
+    )
+  }
+}
