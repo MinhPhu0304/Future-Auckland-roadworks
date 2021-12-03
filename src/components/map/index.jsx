70f91dd6f63b3cf669c1f993150449d7026cfd53
minh <Lminhphu0304@gmail.com>
2021-12-04 09:41:28 +1300

    Literally rewrite map component because of breaking change

--- src/components/map/index.jsx
+++ src/components/map/index.jsx
@@ -1,50 +1,84 @@
-import React, { useState, useEffect } from 'react'
-import  { connect } from 'react-redux'
-import { Map as LeafletMap, TileLayer } from 'react-leaflet'
+import React, { useState, useEffect } from "react";
+import { connect } from "react-redux";
+import { MapContainer, TileLayer, useMapEvents, useMap } from "react-leaflet";
 
-import { Clustering } from 'utils'
-import { Markers } from './marker'
+import { Clustering } from "utils";
+import { Markers } from "./marker";
 
-export const MapRef = React.createRef(null) // For future use
+export const Map = connect(mapStateToProps)(MapDump);
 
-export const Map = connect(mapStateToProps)(MapDump)
-
-function mapStateToProps ({ constructions }) {
+function mapStateToProps({ constructions }) {
   return {
-    constructions
-  }
+    constructions,
+  };
 }
 
-function MapDump ({ constructions }) {
-  const initialMapView = getInitMapView()
-  const position = [initialMapView.lat, initialMapView.lng]
-  const [cluster, setCluster] = useState(Clustering.makeCluster(constructions))
-  const [points, setPoints] = useState([])
-  useEffect(() => {
-    setCluster(Clustering.makeCluster(constructions))
-  }, [constructions])
-
-  useEffect(() => {
-    const bounds = MapRef.current.leafletElement.getBounds()
-    const bbox = [bounds.getWest(), bounds.getSouth(), bounds.getEast(), bounds.getNorth()]
-    if (cluster.points.length > 0) setTimeout(() => setPoints(cluster.getClusters(bbox, initialMapView.zoom)), 50)
-    // eslint-disable-next-line
-  }, [cluster])
-  const handleChange = (viewPort) => {
-    const bounds = MapRef.current.leafletElement.getBounds()
-    const bbox = [bounds.getWest(), bounds.getSouth(), bounds.getEast(), bounds.getNorth()]
-    setPoints(cluster.getClusters(bbox, viewPort.zoom))
-  }
+function MapDump({ constructions }) {
+  const initialMapView = getInitMapView();
+  const position = [initialMapView.lat, initialMapView.lng];
 
   return (
-      <LeafletMap className="map" center={position} zoom={initialMapView.zoom} onViewportChanged={handleChange} ref={MapRef} >
-        <TileLayer
-          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
-        />
-        <Markers points={points}/> 
-      </LeafletMap>
-  )
+    <MapContainer
+      className="map"
+      center={position}
+      zoom={initialMapView.zoom}
+      zoomAnimation={true}
+    >
+      <TileLayer
+        attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+      />
+      <ConstructionPoints constructions={constructions} />
+    </MapContainer>
+  );
+}
+
+function ConstructionPoints({ constructions }) {
+  const initialMapView = getInitMapView();
+  const mapInstance = useMap();
+  const [cluster, setCluster] = useState(Clustering.makeCluster(constructions));
+  const [points, setPoints] = useState([]);
+
+  useEffect(() => {
+    setCluster(Clustering.makeCluster(constructions));
+  }, [constructions]);
+
+  useEffect(() => {
+    const bounds = mapInstance.getBounds();
+    const bbox = [
+      bounds.getWest(),
+      bounds.getSouth(),
+      bounds.getEast(),
+      bounds.getNorth(),
+    ];
+    if (cluster.points.length > 0)
+      setTimeout(
+        () => setPoints(cluster.getClusters(bbox, initialMapView.zoom)),
+        10
+      );
+    // eslint-disable-next-line
+  }, [cluster]);
+
+  const handleMapMove = (e) => {
+    const bounds = mapInstance.getBounds();
+    const bbox = [
+      bounds.getWest(),
+      bounds.getSouth(),
+      bounds.getEast(),
+      bounds.getNorth(),
+    ];
+    setPoints(cluster.getClusters(bbox, e.target._zoom));
+  };
+
+  useMapEvents({
+    move: handleMapMove,
+  });
+
+  return (
+    <>
+      <Markers points={points} />
+    </>
+  );
 }
 
 function getInitMapView() {
@@ -52,5 +86,5 @@ function getInitMapView() {
     lat: -36.848461,
     lng: 174.763336,
     zoom: 10,
-  }
+  };
 }
--- src/components/map/marker.jsx
+++ src/components/map/marker.jsx
@@ -1,67 +1,86 @@
-import React from 'react'
-import { renderToString } from 'react-dom/server'
-import { divIcon } from 'leaflet'
-import PropTypes from 'prop-types'
-import { Marker as LeafletMarker, Popup, withLeaflet } from 'react-leaflet'
+import React from "react";
+import { renderToString } from "react-dom/server";
+import { divIcon } from "leaflet";
+import PropTypes from "prop-types";
+import { Marker as LeafletMarker, Popup, useMap } from "react-leaflet";
 
-export const Markers = withLeaflet(MarkersDump)
-
-function MarkersDump ({ leaflet, points }) {
+export function Markers({ points }) {
+  const mapInstance = useMap();
   const zoomIn = (latlng) => {
-    const currentZoom = leaflet.map.getZoom()
-    leaflet.map.setView(latlng, currentZoom + 3)
-  }
+    const currentZoom = mapInstance.getZoom();
+    mapInstance.setView(latlng, currentZoom + 3);
+  };
   return (
     <>
-      {
-        points.map((location, index) => {
-          if (location.properties.cluster) return (<ClusterMarker location={location} key={index} zoomIn={zoomIn} />)
+      {points.map((location, index) => {
+        if (location.properties.cluster) {
           return (
-            <LeafletMarker key={index} position={[location.geometry.coordinates[1], location.geometry.coordinates[0]]}>
-              <Popup autoPan={false}>
-                {location.properties.location.description.replace("Other (Specify Below). ","")}
-              </Popup>
-            </LeafletMarker>
-          )
-        })}
+            <ClusterMarker location={location} key={index} zoomIn={zoomIn} />
+          );
+        }
+        return (
+          <LeafletMarker
+            key={index}
+            position={[
+              location.geometry.coordinates[1],
+              location.geometry.coordinates[0],
+            ]}
+          >
+            <Popup autoPan={false}>
+              {location.properties.location.description.replace(
+                "Other (Specify Below). ",
+                ""
+              )}
+            </Popup>
+          </LeafletMarker>
+        );
+      })}
     </>
-  )
+  );
 }
 
 Markers.propTypes = {
   points: PropTypes.array.isRequired,
-}
+};
 
-function ClusterMarker ({ location, zoomIn }) {
+function ClusterMarker({ location, zoomIn }) {
   const icon = divIcon({
-    className: 'Cluster',
-    html: renderToString(<ClusterMarkerStyle location={location} />)
-  })
+    className: "Cluster",
+    html: renderToString(<ClusterMarkerStyle location={location} />),
+  });
   return (
-    <LeafletMarker icon={icon} position={[location.geometry.coordinates[1], location.geometry.coordinates[0]]} onClick={() => zoomIn([location.geometry.coordinates[1], location.geometry.coordinates[0]])} >
-    </LeafletMarker>
-  )
+    <LeafletMarker
+      icon={icon}
+      position={[
+        location.geometry.coordinates[1],
+        location.geometry.coordinates[0],
+      ]}
+      eventHandlers={{
+        click: () =>
+          zoomIn([
+            location.geometry.coordinates[1],
+            location.geometry.coordinates[0],
+          ]),
+      }}
+    />
+  );
 }
 
-function ClusterMarkerStyle ({ location }) {
-  const style = makeCustomCircleStyle()
-  return (
-    <div style={style}>
-      {location.properties.point_count}
-    </div>
-  )
+function ClusterMarkerStyle({ location }) {
+  const style = makeCustomCircleStyle();
+  return <div style={style}>{location.properties.point_count}</div>;
 }
 
-function makeCustomCircleStyle () {
+function makeCustomCircleStyle() {
   return {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
     height: 25,
     width: 25,
-    backgroundColor: 'black',
-    color: 'white',
-    borderRadius: '50%',
-    border: '3px double red'
-  }
+    backgroundColor: "black",
+    color: "white",
+    borderRadius: "50%",
+    border: "3px double red",
+  };
 }
