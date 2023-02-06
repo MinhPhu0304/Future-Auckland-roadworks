158774f16d49629399a7aaa5b5c16d76de1878d2
minh <Lminhphu0304@gmail.com>
2023-02-06 15:56:05 +0700

    Dump data instead

--- src/store/thunks/getAllConstructions.js
+++ src/store/thunks/getAllConstructions.js
@@ -1,25 +1,31 @@
-import { Constructions, PageState } from 'store/reducers'
-import { PAGE_STATE } from 'store/reducers/pageState'
+import { Constructions, PageState } from "store/reducers";
+import { PAGE_STATE } from "store/reducers/pageState";
 
 function getAllConstructions() {
   return async (dispatch) => {
     try {
-      const res = await fetch('https://api.at.govt.nz/v2/locations/scheduledworks', {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          'Ocp-Apim-Subscription-Key': `${process.env.REACT_APP_AT_TRANSPORT_KEY}`
+      const res = await fetch(
+        "https://raw.githubusercontent.com/MinhPhu0304/data-dump/main/at-roadworks.json",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
         }
-      })
-      const { response } = await res.json()
-      dispatch(Constructions.actions.UPDATE_CONSTRUCTIONS({ constructions: response }))
-      dispatch(PageState.actions.UPDATE_PAGE_STATE({ newPageState: PAGE_STATE.LOADED }))
+      );
+      const { response } = await res.json();
+      dispatch(
+        Constructions.actions.UPDATE_CONSTRUCTIONS({ constructions: response })
+      );
+      dispatch(
+        PageState.actions.UPDATE_PAGE_STATE({ newPageState: PAGE_STATE.LOADED })
+      );
     } catch (e) {
-      dispatch(PageState.actions.UPDATE_PAGE_STATE({ newPageState: PAGE_STATE.FAILED }))
+      dispatch(
+        PageState.actions.UPDATE_PAGE_STATE({ newPageState: PAGE_STATE.FAILED })
+      );
     }
-  }
+  };
 }
 
-export {
-  getAllConstructions,
-}
+export { getAllConstructions };
